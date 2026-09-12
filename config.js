06cb786a931710eab7160f1623bd1571142e3af9
dedeikibaconboy <rizky@silverhawk.web.id>
2026-09-12 12:05:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
  *
  * Lihat SETUP.md untuk cara mendapatkannya.
  */
-window.MONMON_API = '';
+window.MONMON_API = 'https://script.google.com/macros/s/AKfycbwMWGGj2SGH7L1SnSo7XxkRAkQX9qbhm9P9i-SfBGPEw4cTa70QNdW7Trp0bfhD_BVevQ/exec';
