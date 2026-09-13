d09383470b58bd5df4da86684559bd32ce492dcc
dedeikibaconboy <rizky@silverhawk.web.id>
2026-09-13 11:15:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@
 window.MONMON_API = 'https://script.google.com/macros/s/AKfycbwMWGGj2SGH7L1SnSo7XxkRAkQX9qbhm9P9i-SfBGPEw4cTa70QNdW7Trp0bfhD_BVevQ/exec';
 
 window.MONMON_FIREBASE = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyCqo53UyMaYR3o0PTHRHg2imH8GHNBsMpM',
+  authDomain: 'monmon-shatter.firebaseapp.com',
+  databaseURL: 'https://monmon-shatter-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'monmon-shatter',
+  storageBucket: 'monmon-shatter.firebasestorage.app',
+  messagingSenderId: '468910132392',
+  appId: '1:468910132392:web:48089a0d3a79c1c882869d'
 };
